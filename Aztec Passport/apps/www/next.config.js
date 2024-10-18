e6905bf068be5b7b58e21f86b96ca8ea788e6fd7
Vedant Chainani <vedantchainani1084@gmail.com>
2024-10-18 14:42:01 +0530

    chore: add twitter verify contracts

--- Aztec Passport/apps/www/next.config.js
+++ Aztec Passport/apps/www/next.config.js
@@ -1,9 +1,35 @@
+import webpack from 'webpack';
+
 await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          // fixes proxy-agent dependencies
+          net: false,
+          dns: false,
+          tls: false,
+          assert: false,
+          // fixes next-i18next dependencies
+          path: false,
+          fs: false,
+          // fixes mapbox dependencies
+          events: false,
+          // fixes sentry dependencies
+          process: false,
+        },
+      };
+    }
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, '');
+      })
+    );
     return config;
   },
 };
