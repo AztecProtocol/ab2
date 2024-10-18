dfabb66515e0848dd29350572d447051d9716c2e
Vedant Chainani <vedantchainani1084@gmail.com>
2024-10-18 22:28:37 +0530

    chore: add social verify mockups

---
+++ Aztec Passport/apps/www/src/app/dashboard/_components/social-verify/github.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+
+import React, { useState } from 'react';
+
+import GitHubLogo from 'public/assets/github.svg';
+
+import { StepButton } from '~/components/ui/step-button';
+
+import { Loader2Icon } from 'lucide-react';
+
+export const GitHubVerify = () => {
+  const [status, setStatus] = useState<'idle' | 'loading' | 'complete'>('idle');
+
+  const onVerify = () => {
+    setStatus('loading');
+    setTimeout(() => {
+      setStatus('complete');
+    }, 3000);
+    setTimeout(() => {
+      setStatus('idle');
+    }, 5000);
+  };
+
+  return (
+    <div className='flex flex-col gap-2 rounded-2xl border border-neutral-300 p-4 text-neutral-200'>
+      <Image
+        alt='Github Logo'
+        className='my-2 h-[2rem] w-[2rem]'
+        src={GitHubLogo as unknown as string}
+      />
+      <div className='text-xl font-medium'>GitHub</div>
+      <p className='text-sm font-medium text-neutral-400'>
+        Verify your GitHub account.
+      </p>
+      <StepButton
+        className='!dark mt-6 h-9 font-semibold !text-black'
+        currentMode={status}
+        finalContent={
+          <div className='flex flex-row items-center justify-center gap-2'>
+            <div>✅</div> Verified
+          </div>
+        }
+        initialContent={
+          <div className='flex flex-row items-center justify-center gap-2'>
+            Verify
+          </div>
+        }
+        loadingContent={
+          <div className='flex flex-row items-center justify-center gap-1'>
+            <Loader2Icon className='animate-spin' size={18} />
+            Verifying...
+          </div>
+        }
+        variants={{
+          initial: { y: '-120%' },
+          animate: { y: '0%' },
+          exit: { y: '120%' },
+        }}
+        onClick={onVerify}
+      />
+    </div>
+  );
+};
