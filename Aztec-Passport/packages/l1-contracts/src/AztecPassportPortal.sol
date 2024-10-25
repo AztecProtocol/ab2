2acb42b96779122d5c1c642d4f3527e20c27d34e
Vedant Chainani <vedantchainani1084@gmail.com>
2024-10-25 13:27:31 +0530

    chore: update passport tests

--- Aztec-Passport/packages/l1-contracts/src/AztecPassportPortal.sol
+++ Aztec-Passport/packages/l1-contracts/src/AztecPassportPortal.sol
@@ -10,19 +10,25 @@ import {DataStructures} from "@aztec/l1-contracts/core/libraries/DataStructures.
 
 // Services
 import {MockENS} from "./mocks/MockENS.sol";
+import {UltraVerifier} from "./verifiers/KYCAge.sol";
 
 contract AztecPassportPortal {
     IRegistry public registry;
     MockENS public ens;
+    UltraVerifier public verifier;
     bytes32 public l2Bridge;
+
+    bytes32 public _lastMessageId;
 
     error NoENSFound();
     error NotEnoughBalance();
+    error KYCAgeVerificationFailed();
 
-    constructor(address _registry, bytes32 _l2Bridge, address _ens) {
+    constructor(address _registry, bytes32 _l2Bridge, address _ens, address _verifier) {
         registry = IRegistry(_registry);
         l2Bridge = _l2Bridge;
         ens = MockENS(_ens);
+        verifier = UltraVerifier(_verifier);
     }
 
     function verifyENS(bytes32 _secretHash) public {
@@ -46,10 +52,27 @@ contract AztecPassportPortal {
         _passMessageToL2(content, _secretHash);
     }
 
-    function _passMessageToL2(bytes32 _content, bytes32 _secretHash) internal {
+    function verifyAge(bytes memory proof, bytes32 _secretHash) public {
+        // Age Claim in JWT using verifier
+        bytes32[] memory publicInputs = new bytes32[](2);
+        publicInputs[0] = bytes32(uint256(1729830622));
+        publicInputs[1] = bytes32(uint256(1));
+
+        bool verified = verifier.verify(proof, publicInputs);
+        if (!verified) {
+            revert KYCAgeVerificationFailed();
+        }
+
+        bytes32 content = bytes32(uint256(1));
+        _passMessageToL2(content, _secretHash);
+    }
+
+    function _passMessageToL2(bytes32 _content, bytes32 _secretHash) internal returns (bytes32) {
         IInbox inbox = IRollup(registry.getRollup()).INBOX();
         DataStructures.L2Actor memory actor = DataStructures.L2Actor(l2Bridge, 1);
 
-        inbox.sendL2Message(actor, _content, _secretHash);
+        bytes32 messageId = inbox.sendL2Message(actor, _content, _secretHash);
+        _lastMessageId = messageId;
+        return messageId;
     }
 }
--- Aztec-Passport/packages/l1-contracts/test/Passport.t.sol
+++ Aztec-Passport/packages/l1-contracts/test/Passport.t.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.13;
 import {Test, console2 as console, Vm} from "forge-std/Test.sol";
 import {AztecPassportPortal} from "src/AztecPassportPortal.sol";
 import {MockENS} from "src/mocks/MockENS.sol";
+import {UltraVerifier} from "src/verifiers/KYCAge.sol";
 
 contract CounterTest is Test {
     AztecPassportPortal public portal;
     MockENS public ens;
+    UltraVerifier public verifier;
     Vm.Wallet public owner;
     Vm.Wallet public alice;
     Vm.Wallet public bob;
@@ -18,8 +20,9 @@ contract CounterTest is Test {
         bob = vm.createWallet("bob");
 
         ens = new MockENS();
-        address registry = address(0);
-        bytes32 l2Bridge = bytes32(0);
-        portal = new AztecPassportPortal(registry, l2Bridge, address(ens));
+        address registry = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+        bytes32 l2Bridge = bytes32(0x1abe6c7f5c4caf04cbf7556495e08ad9c0a225a5f9d33554ae07285b13c494d7);
+        verifier = new UltraVerifier();
+        portal = new AztecPassportPortal(registry, l2Bridge, address(ens), address(verifier));
     }
 }
