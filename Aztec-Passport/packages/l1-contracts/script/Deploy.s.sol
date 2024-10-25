06ed5c8a2e9e1f907027e8dbda3c309ae23833f3
Vedant Chainani <vedantchainani1084@gmail.com>
2024-10-25 11:17:41 +0530

    chore: add solidity verifier

--- Aztec-Passport/packages/l1-contracts/script/Deploy.s.sol
+++ Aztec-Passport/packages/l1-contracts/script/Deploy.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "src/Counter.sol";
 
 contract DeployScript is Script {
     function setUp() public {}
@@ -12,11 +11,6 @@ contract DeployScript is Script {
         address deployerAddress = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
 
-        console.log("Deploying Counter with deployer address", deployerAddress);
-
-        Counter counter = new Counter(deployerAddress);
-
-        console.log("Deployed Counter at address: %s", address(counter));
         vm.stopBroadcast();
     }
 }
