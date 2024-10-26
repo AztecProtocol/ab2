2d7df337070aeee767b76df8f92ef8711d6a03f4
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-10-27 02:24:36 +0300

    ZeKshop's verify purchase circuit now support custom purchase numbers and custom email sizes of their senders and recipients. Improved circuit and client enormously. Circuit compiles successfully and test is passing also successfully.

--- zekshop/circuits/client/tests/circuits.test.ts
+++ zekshop/circuits/client/tests/circuits.test.ts
@@ -4,18 +4,43 @@ import { ZKEmailProver } from "../src/prover";
 import { generateEmailVerifierInputs } from "../src/index";
 import circuitPurchaseNumber from "../../verify_purchase/target/verify_purchase.json";
 import { toProverToml } from "../src/utils";
-import { Uint8ArrayToCharArray } from "@zk-email/helpers";
 
-let utf8Encode = new TextEncoder();
-const purchaseNumber = Uint8ArrayToCharArray(utf8Encode.encode("1294352407"));
 const email = {
     data: fs.readFileSync(path.join(__dirname, "./test-data/attempt_without_exclamation.eml")),
 };
 
-const inputParams = {
-    maxHeadersLength: 576,
-    maxBodyLength: 1216,
-    purchaseNumber
+const fromEmailString = "nikossta4@gmail.com";
+const toEmailString = "nikolaykostadinov21@gmail.com";
+const purchaseNumberString = "1294352407";
+const headerLength = 574;
+const maxHeadersLength = 576;
+const maxBodyLength = 1216;
+
+const fromEmailAddressPadded = new Uint8Array(60);
+fromEmailAddressPadded.set(
+    Uint8Array.from(new TextEncoder().encode(fromEmailString))
+);
+
+const toEmailAddressPadded = new Uint8Array(60);
+toEmailAddressPadded.set(
+    Uint8Array.from(new TextEncoder().encode(toEmailString))
+);
+
+const purchaseNumberPadded = new Uint8Array(20);
+purchaseNumberPadded.set(
+    Uint8Array.from(new TextEncoder().encode(purchaseNumberString))
+);
+
+const correctInputParams = {
+    maxHeadersLength,
+    maxBodyLength,
+    headerLength: headerLength.toString(),
+    purchaseNumber: Array.from(purchaseNumberPadded).map((s) => s.toString()),
+    purchaseNumberLength: purchaseNumberString.length.toString(),
+    fromEmailAddress: Array.from(fromEmailAddressPadded).map((s) => s.toString()),
+    fromEmailAddressLength: fromEmailString.length.toString(),
+    toEmailAddress: Array.from(toEmailAddressPadded).map((s) => s.toString()),
+    toEmailAddressLength: toEmailString.length.toString(),
 };
 
 describe("Email verification tests", () => {
@@ -30,12 +55,10 @@ describe("Email verification tests", () => {
         await proverPurchaseNumber.destroy();
     });
     it("Verify successful purchase", async () => {
-        console.log('purchaseNumber', purchaseNumber);
         const inputs = await generateEmailVerifierInputs(
             email.data,
-            inputParams
+            correctInputParams
         );
         await proverPurchaseNumber.simulateWitness(inputs);
-        console.log(toProverToml(inputs));
     });
 });
