aa9ff2251e1ea7d7011013b4ea1ef30ebc0f4977
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-10-31 00:38:47 +0200

    fix: Remove redundant code and sanitize prover_utils.ts

--- zekshop/app/app/opinion/Opinion.tsx
+++ zekshop/app/app/opinion/Opinion.tsx
@@ -7,24 +7,16 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import DragAndDropFile from "./DragAndDropFile";
-import { file2Base64, verifyProof } from "../prover_utils";
+import { verifyProof } from "../prover_utils";
 
 function Opinion() {
   const [open, setOpen] = useState(false);
-
-  // Opens the dialog
   const handleOpen = () => setOpen(true);
-
-  // Closes the dialog
   const handleClose = () => setOpen(false);
 
   // Handler for when a file is selected in the DragAndDropFile component
   const handleFileSelect = async (file: File) => {
-    console.log("handleFileSelect");
-    console.log("Selected file:", file);
-    const file_in_base64 = await file2Base64(file);
-    const x = await verifyProof(file_in_base64, file);
-    console.log("x: ", x);
+    await verifyProof(file);
     handleClose();
   };
 
--- zekshop/app/app/prover_utils.ts
+++ zekshop/app/app/prover_utils.ts
@@ -1,17 +1,7 @@
 import { generateEmailVerifierInputs } from "@mach-34/zkemail-nr";
 import { type Noir, type CompiledCircuit, InputMap } from "@noir-lang/noir_js";
-import {
-    BarretenbergBackend,
-    BarretenbergVerifier,
-    ProofData,
-    UltraHonkBackend,
-    UltraHonkVerifier,
-} from "@noir-lang/backend_barretenberg";
-import circuitPurchaseNumber from "./assets/verify_purchase.json";
-import { AddressObject, simpleParser } from 'mailparser';
-import fs from 'fs';
-import { ZKEmailProver } from "./proving_helpers/prover";
-import path from "path";
+import { UltraHonkBackend } from "@noir-lang/backend_barretenberg";
+import { simpleParser } from 'mailparser';
 
 interface ParsedEmailInfo {
     fromEmail: string;
@@ -31,20 +21,7 @@ type UltraHonkBackendModules = {
     circuit: object;
 }
 
-type VerifierModules = {
-    UltraHonkVerifier: typeof UltraHonkVerifier;
-    vkey: number[];
-};
-
-type BarretenbergVerifierModules = {
-    BarretenbergVerifier: typeof BarretenbergVerifier;
-    vkey: number[];
-}
-
-// const text = fs.readFileSync('tests/test-data/attempt_without_exclamation.eml', 'utf8')
 let proverPromise: Promise<ProverModules> | null = null;
-let verifierPromise: Promise<VerifierModules> | null = null;
-let barretenbergVerifierPromise: Promise<BarretenbergVerifierModules> | null = null;
 let ultraHonkBackendPromise: Promise<UltraHonkBackendModules> | null = null;
 
 // Lazy load prover libs to avoid initial page load delay
@@ -76,36 +53,12 @@ export async function initVerifier(): Promise<UltraHonkBackendModules> {
     return ultraHonkBackendPromise;
 }
 
-// export async function verifyProofUltraHonk(
-//     proof: Uint8Array,
-//     publicInputs: string[]
-// ): Promise<boolean> {
-//     await initVerifier();
-
-//     const { Noir, UltraHonkBackend, circuit } = await initVerifier();
-
-//     const proofData = {
-//         proof: proof,
-//         publicInputs,
-//     };
-
-//     const verifier = new BarretenbergBackend(proofData);
-//     const result = await verifier.verifyProof(proofData);
-
-//     return result;
-// }
-
 async function extractEmailDetailsFromFile(file: File): Promise<ParsedEmailInfo> {
     // Read the file as an ArrayBuffer
     const emlBuffer = await file.arrayBuffer();
 
-    // Parse the .eml content using mailparser
     const email = await simpleParser(Buffer.from(emlBuffer));
-
-    // Extract 'from' email address
     const fromEmail = email.from?.value[0]?.address || 'No sender found';
-
-    // Extract 'to' email address
     const toEmail = (email.to as any).value[0]?.address || 'No recipient found';
 
     // First, try to extract Purchase number from the headers
@@ -120,6 +73,7 @@ async function extractEmailDetailsFromFile(file: File): Promise<ParsedEmailInfo>
         purchaseNumber
     };
 }
+
 // Helper function to extract Purchase number from headers
 function extractPurchaseNumberFromHeader(headers: Map<string, string>): string | null {
     const subject = headers.get('subject') || '';
@@ -133,20 +87,10 @@ function extractPurchaseNumberFromBody(content: string): string | null {
     return match ? match[1] : null;
 }
 
-export const file2Base64 = (file: File): Promise<string> => {
-    return new Promise<string>((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result?.toString() || '');
-        reader.onerror = error => reject(error);
-    })
-}
-
 async function getEmailContent(file: File): Promise<string> {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
 
-        // Event listener for successfully reading the file
         reader.onload = () => {
             if (typeof reader.result === 'string') {
                 resolve(reader.result);  // Return the raw content as a string
@@ -155,7 +99,6 @@ async function getEmailContent(file: File): Promise<string> {
             }
         };
 
-        // Event listener for errors
         reader.onerror = () => {
             reject('Error reading file');
         };
@@ -165,17 +108,9 @@ async function getEmailContent(file: File): Promise<string> {
     });
 }
 
-export async function verifyProof(_emailString: string, emailContent: File) {
-    const headerLength = 574;
-    const maxHeadersLength = 576;
-    const maxBodyLength = 1216;
-
+export async function verifyProof(emailContent: File) {
     const emailDetails = await extractEmailDetailsFromFile(emailContent);
-    console.log(emailDetails)
-    console.log('emailDetails', emailDetails);
-
     const emailString = await getEmailContent(emailContent);
-    console.log('Email Content:', emailContent)
 
     // Generate common inputs using ZK Email SDK
     const zkEmailInputs = await generateEmailVerifierInputs(emailString, {
@@ -198,6 +133,7 @@ export async function verifyProof(_emailString: string, emailContent: File) {
         Uint8Array.from(new TextEncoder().encode((emailDetails).purchaseNumber as any))
     );
 
+    const headerLength = 574;
     const correctInputParams = {
         ...zkEmailInputs,
         header_length: headerLength.toString(),
@@ -208,10 +144,6 @@ export async function verifyProof(_emailString: string, emailContent: File) {
         to_email_address: Array.from(toEmailAddressPadded).map((s) => s.toString()),
         to_email_address_length: (emailDetails).toEmail?.length,
     };
-
-    console.log('correctInputParams', correctInputParams)
-    console.log('inputs: ', zkEmailInputs)
-
 
     const { Noir, UltraHonkBackend, circuit } = await initProver();
 
@@ -234,82 +166,4 @@ export async function verifyProof(_emailString: string, emailContent: File) {
     console.log('Starting to verify...')
     const x = await backend.verifyProof({ proof, publicInputs });
     console.log('Verification result:', x);
-
-    // let prover = new ZKEmailProver(circuitPurchaseNumber as any, "all");
-    // console.log('prover', prover)
-    // const proof = await prover.fullProve(zkEmailInputs);
-    // console.log('proof', proof)
-    // const result = await prover.verify(proof);
-
-    // console.log('result', result)
-
-    // return result;
-}
-
-export async function generateProof(
-    emailContent: string
-) {
-    try {
-
-        // Generate common inputs using ZK Email SDK
-        const zkEmailInputs = await generateEmailVerifierInputs(emailContent, {
-            maxBodyLength: 1216, // Same as MAX_PARTIAL_EMAIL_BODY_LENGTH in circuit
-            maxHeadersLength: 576, // Same as MAX_EMAIL_HEADER_LENGTH in circuit
-        });
-
-        const fromEmailString = "nikossta4@gmail.com";
-        const toEmailString = "nikolaykostadinov21@gmail.com";
-        const purchaseNumberString = "1294352407";
-
-        const fromEmailAddressPadded = new Uint8Array(60);
-        fromEmailAddressPadded.set(
-            Uint8Array.from(new TextEncoder().encode(fromEmailString))
-        );
-
-        const toEmailAddressPadded = new Uint8Array(60);
-        toEmailAddressPadded.set(
-            Uint8Array.from(new TextEncoder().encode(toEmailString))
-        );
-
-        const purchaseNumberPadded = new Uint8Array(20);
-        purchaseNumberPadded.set(
-            Uint8Array.from(new TextEncoder().encode(purchaseNumberString))
-        );
-
-
-
-        const inputs = {
-            ...zkEmailInputs,
-            header: zkEmailInputs.header,
-            header_length: zkEmailInputs.header.len,
-            body: zkEmailInputs.body,
-            pubkey: zkEmailInputs.pubkey,
-            signature: zkEmailInputs.signature,
-            purchase_number: Array.from(purchaseNumberPadded).map((s) => s.toString()),
-            purchase_number_length: purchaseNumberString.length.toString(),
-            from_email_address: Array.from(fromEmailAddressPadded).map((s) => s.toString()),
-            from_email_address_length: fromEmailString.length.toString(),
-            to_email_address: Array.from(toEmailAddressPadded).map((s) => s.toString()),
-            to_email_address_length: toEmailString.length.toString(),
-        };
-
-        console.log("Generating proof with inputs:", inputs);
-
-        const { Noir, UltraHonkBackend, circuit } = await initProver();
-
-        // Initialize Noir JS
-        const backend = new UltraHonkBackend(circuit as CompiledCircuit);
-        const noir = new Noir(circuit as CompiledCircuit);
-
-        // Generate witness and prove
-        const startTime = performance.now();
-        const { witness } = await noir.execute(inputs as InputMap);
-        const proofResult = await backend.generateProof(witness);
-        const provingTime = performance.now() - startTime;
-
-        return { ...proofResult, provingTime };
-    } catch (error) {
-        console.error("Error generating proof:", error);
-        throw new Error("Failed to generate proof");
-    }
 }
