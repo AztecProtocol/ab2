f2b819ea7e917c7d89379e56b425150fc7fcf98a
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-10-31 21:49:41 +0200

    feat: Improve the scroll bar and making the scrolling smooth

--- zekshop/app/app/page.tsx
+++ zekshop/app/app/page.tsx
@@ -9,9 +9,40 @@ import {
 } from "./background/background";
 import { CardItem, CardContainer } from "./product_items/card_item";
 
+import SmoothScroll from "smooth-scroll";
+
+// Initialize smooth scroll with options
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 600, // Adjust speed to your preference
+  speedAsDuration: true,
+});
+
 export default function Home() {
+  const css = `
+      html {
+      scroll-behavior: smooth;
+    }
+
+    /* Custom scrollbar styling */
+    ::-webkit-scrollbar {
+      width: 5px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: #1f1f1f;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: #9854bf;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background-color: #c02c63;
+    }
+  `;
   return (
     <div>
+      <style>{css}</style>
       <GlobalStyle />
       <NavbarOverlay>
         <Navbar />
